6301165a82eb8a3fa60f8d6b7412eb54023c45a4
shenoyganesprasad <shenoyganeshprasad@gmail.com>
2019-10-27 17:25:34 +0530

    add venueNfo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './resources/styles.css'
 
 import Header from './components/header-footer/Header'
 import Featured from './components/featured'
+import VenueNfo from './components/venueNFO'
 
 function App() {
   return (
     <div className="App" style={{height: '1500px', backgroundColor: 'cornflowerblue'}}>
       <Header />
       <Featured />
+      <VenueNfo />
     </div>
   );
 }
