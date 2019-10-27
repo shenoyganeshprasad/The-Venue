a41dcfea4dffe1564ab22be297eceae6873804b1
shenoyganesprasad <shenoyganeshprasad@gmail.com>
2019-10-27 19:23:24 +0530

    Updates

---
+++ precache-manifest.1cd97fa1a33f6279e29a9bc686538c6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2233c39711fd62d294ebce5812e3746e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa0dd9ff101f3a715b20",
+    "url": "/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "455cb276dcc4ac4d296c",
+    "url": "/static/js/2.608d3ae6.chunk.js"
+  },
+  {
+    "revision": "aa0dd9ff101f3a715b20",
+    "url": "/static/js/main.fa9c6f4c.chunk.js"
+  },
+  {
+    "revision": "6515a9f2f4dc3485d886",
+    "url": "/static/js/runtime-main.252c81bd.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
