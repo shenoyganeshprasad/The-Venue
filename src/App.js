f8e4d84292936e092cc9fc03c59aa8777b9db8b1
shenoyganesprasad <shenoyganeshprasad@gmail.com>
2019-10-27 18:35:04 +0530

    add map

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Featured from './components/featured'
 import VenueNfo from './components/venueNFO'
 import Highlight from './components/highlights'
 import Pricing from './components/pricing'
+import Location from './components/location'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <VenueNfo />
       <Highlight />
       <Pricing />
+      <Location />
     </div>
   );
 }
---
+++ src/components/location/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Location = () => {
+      return (
+            <div className="location_wrapper">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3354.4552939575424!2d-96.80127438505087!3d32.78018508097102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e9918e993c711%3A0xa9073cfe6d9a606a!2sMagnolia%20Dallas%20Downtown!5e0!3m2!1sen!2sin!4v1572181240736!5m2!1sen!2sin" width="100%" height="500px" frameB order="0" allowFullScreen></iframe>
+                  <div className="location_tag">
+                        <div>Location</div>
+                  </div>
+            </div>
+      );
+};
+
+export default Location;
